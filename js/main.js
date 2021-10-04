f0ed8ab9f9a26690ac8415a3c336539205a2da35
bob <vova_progmush@mail.ru>
2021-10-04 16:52:28 +0300

    sixth commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,69 @@ hamburger.addEventListener('click', function() {
   menu.classList.toggle('active');
 
 });
+
+
+// Slider
+
+class Slider {
+  elements;
+  buttons;
+  position = 0;
+  movePosition;
+  maxPosition;
+
+  constructor(wrapper, config) {
+      this.elements = {
+          container: wrapper.querySelector('.slider__container'),
+          track: wrapper.querySelector('.slider__track'),
+          items: wrapper.querySelectorAll('.slider__item')
+      };
+
+      this.buttons = {
+          prev: wrapper.querySelector('.btn__prev'),
+          next: wrapper.querySelector('.btn__next'),
+      };
+
+      const itemWidth = this.elements.container.clientWidth / config.slidesToShow
+      this.movePosition = config.slidesToScroll * itemWidth;
+      this.maxPosition = -(this.elements.items.length * itemWidth - config.slidesToShow * itemWidth);
+      this.elements.items.forEach((item) => item.style.minWidth = `${itemWidth}px`);
+      this.buttons.prev.addEventListener('click', () => this.handlePrevClick());
+      this.buttons.next.addEventListener('click', () => this.handleNextClick());
+      this.checkBtns();
+  }
+
+  handlePrevClick() {
+      this.position += this.movePosition;
+      if(this.position > 0) {
+          this.position = 0;
+      }
+      this.setPosition();
+      this.checkBtns();
+  }
+
+  handleNextClick() {
+      this.position -= this.movePosition;
+      if(this.position < this.maxPosition) {
+          this.position = this.maxPosition;
+      }
+      this.setPosition();
+      this.checkBtns();
+  }
+
+  setPosition() {
+      this.elements.track.style.transform = `translateX(${this.position}px)`
+  }
+
+  checkBtns() {  
+      this.buttons.prev.disabled = this.position === 0;
+      this.buttons.next.disabled = this.position <= this.maxPosition;
+  }
+}
+
+new Slider(document.querySelector('.widgets__wrapper'), {
+slidesToShow: 2,
+slidesToScroll: 1
+});
+
+
